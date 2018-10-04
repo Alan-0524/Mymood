2fb323e43ab7891bef114ca21589b6c31604a475
Alan-0524 <119241066@163.com>
2018-10-04 19:19:33 +1300

    update

--- Mymood/messenger_utility.py
+++ Mymood/messenger_utility.py
@@ -8,17 +8,15 @@ def push_notifications():
     http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
     apiUrl = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAE358wDWxQBANcfxlUa3tfs7DPhsrrtJN8Q8AWvE7tvq3RdErlFyfHDKY8SMZBHR4YTK8fJkxW054ZAKDDfgXVkXzb0VPvj9gg2ZBs7rBI8k6Yt263vaxGMlBmsxks5oWfkiT7w2Uh5LiCeAll3lYfvpT7oFedMTiHaQMp7l60yVjJvhZAc'
     recipient = {'id': '2334765856551775'}
-    # message = {'text': {'🤣', '😁', '😃','🙂','😐', '😔','😞', '😭','👿'}}
-    # message = {'text': 'hello,world'}
-    message = {'attachment': {'type': 'template', 'payload': {'template_type': 'button', 'text': 'Try the URL button!',
-                                                              'buttons': [{
-                                                                  "type": "web_url",
-                                                                  "url": "https://mymood-service.herokuapp.com/select_emoji/",
-                                                                  "title": "URL Button",
-                                                                  "webview_height_ratio": "compact",
-                                                                  "messenger_extensions": "true",
-                                                                  # "fallback_url": "https://mymood-service.herokuapp.com/select_emoji/"
-                                                              }]}}}
+    message = {
+        'attachment': {'type': 'template', 'payload': {'template_type': 'button', 'text': 'Hi! How are you going?😁',
+                                                       'buttons': [{
+                                                           "type": "web_url",
+                                                           "url": "https://mymood-service.herokuapp.com/select_emoji/",
+                                                           "title": "Tell me 👇",
+                                                           "webview_height_ratio": "full",
+                                                           "messenger_extensions": "true",
+                                                       }]}}}
     # message = {"text": "how are you going?", "quick_replies": [
     #     {"content_type": "text", "title": "🤣", "payload": "0"},
     #     {"content_type": "text", "title": "😁", "payload": "1"},
