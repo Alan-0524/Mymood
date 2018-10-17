2ba2653e53cdb06ccbfa3d1d6161a03df24b9ec2
SD <41408392+oox511@users.noreply.github.com>
2018-10-17 15:43:27 +1300

    Update process_events.py

--- Mymood/biz/biz-view/process_events.py
+++ Mymood/biz/biz-view/process_events.py
@@ -1,5 +1,22 @@
 from models_app.models import *
+import uuid
+import html
 
+
+def query_events():
+    # Get all objects
+    event_list = TblEvent.objects.all()
+    text = str("")
+    # Loop list
+    for i in range(0, len(event_list)):
+        # put objects in string of Html
+        event = event_list.__getitem__(i)
+        text = text + "<tr><td>" + event.event_title + "</td><td>" + str(event.event_date)[
+
+                                                                     :10] + "</td><td>" + event.event_content + "</td></tr>"
+    # Translation string to html
+    html_text = html.unescape(text)
+    return html_text
 
 
 def save_event(request):
@@ -8,7 +25,7 @@ def save_event(request):
         event.id = str(uuid.uuid1()).replace("-", "")
         event.event_title = request.POST.get('event_title')
         event.event_date = request.POST.get('event_time')
-        event.evenequest.POST.get('event_content')
+        event.event_content = request.POST.get('event_content')
         event.save()
     except Exception as e:
         print("Error info:----------------", e)
