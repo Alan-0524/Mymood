4e660f7489b8c94ffb4e1495857074850b751277
SD <41408392+oox511@users.noreply.github.com>
2018-10-17 15:12:26 +1300

    Update process_teams.py

--- Mymood/biz/biz-view/process_teams.py
+++ Mymood/biz/biz-view/process_teams.py
@@ -1,5 +1,42 @@
 from models_app.models import *
-imp
+import uuid
+from django.db.models import Max
+import random
+import html
+
+
+def query_all_teams(request):
+    team_list = TblTeam.objects.all()
+    return team_list
+
+
+def create_teams(request):
+    try:
+        team_name = request.POST.get("team_name")
+        team = TblTeam()
+        team.id = str(uuid.uuid1()).replace("-", "")
+        new_id = random.randint(0, 999999)
+        check_team_id = TblTeam.objects.filter(team_id=new_id)
+        if check_team_id.exists():
+            new_id = random.randint(0, 999999)
+        else:
+            team.team_id = new_id
+        team.name = team_name
+        team.save()
+    except Exception as e:
+        print("Error info:----------------", e)
+        return "error"
+    return "success"
+
+
+def check_team_name(request):
+    team_name = request.POST.get("team_name")
+    team = TblTeam.objects.filter(name=team_name)
+    if team.exists():
+        return True
+    else:
+        return False
+
 
 def query_teams_html(request):
     list_query_teams = TblTeam.objects.all()
