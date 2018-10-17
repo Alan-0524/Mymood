a020f578218daf1b62faa8dbb3ebbeba4fe58d05
Alan-0524 <119241066@163.com>
2018-10-17 23:41:11 +1300

    update timed task

--- models_app/management/commands/timed_task.py
+++ models_app/management/commands/timed_task.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
+from Mymood import messenger_utility
 import schedule
 import time
 
@@ -7,9 +8,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         def job():
             print("I'm working...")
+            messenger_utility.push_notifications("2334765856551775")
 
         # schedule.every(1).seconds.do(job)
-        schedule.every(5).seconds.do(job)
+        schedule.every(10).seconds.do(job)
         # schedule.every().hour.do(job)
         # schedule.every().day.at("22:03").do(job)
         # schedule.every(5).to(10).days.do(job)
