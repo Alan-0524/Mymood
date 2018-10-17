8d8f4bdba1d1d5a5f5803b31531692b177d75e82
Alan-0524 <119241066@163.com>
2018-10-17 23:28:31 +1300

    update timed task

--- models_app/management/commands/timed_task.py
+++ models_app/management/commands/timed_task.py
@@ -8,8 +8,8 @@ class Command(BaseCommand):
         def job():
             print("I'm working...")
 
-        schedule.every(1).seconds.do(job)
-        # schedule.every(5).seconds.do(job)
+        # schedule.every(1).seconds.do(job)
+        schedule.every(5).seconds.do(job)
         # schedule.every().hour.do(job)
         # schedule.every().day.at("22:03").do(job)
         # schedule.every(5).to(10).days.do(job)
